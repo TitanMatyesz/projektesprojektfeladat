694e941eaa98b350d9007e09eb90aeb3e48a1b1d
Titán Matyesz <116081262+TitanMatyesz@users.noreply.github.com>
2024-03-21 17:30:28 +0100

    Add files via upload

--- ugyfelszolgalat.py
+++ ugyfelszolgalat.py
@@ -37,3 +37,28 @@ for hivas in hivasok:
     hivasSorszam += 1
 
 print(f"A legtovább tartó hívás a {maxHivasSorszam} sorban van, a hívás {maxhivas} másodperc hosszú volt.")
+
+print("5. feladat")
+idopont = input("Kérek egy időpontot!(óra, perc, másodperc)")
+idopont = list(map(int, idopont.strip().split(" ")))
+idoMasodperc = mpbe(idopont[0], idopont[1], idopont[2])
+
+i = 0
+
+while i < len(hivasok) and not(mpbe(hivasok[i][0], hivasok[i][1], hivasok[i][2]) <= idoMasodperc and idoMasodperc < mpbe(hivasok[i][3], hivasok[i][4], hivasok[i][5])):
+    i += 1
+
+if i < len(hivasok):
+    telefonalo = i + 1
+else:
+    telefonalo = 0
+
+if telefonalo:
+    varakozok = -1
+    for hivas in hivasok:
+        if mpbe(hivas[0], hivas[1], hivas[2]) <= idoMasodperc and idoMasodperc < mpbe(hivas[3], hivas[4], hivas[5]) :
+            varakozok +=1
+    print(f"A várakozók száma: {varakozok} a beszélő a {telefonalo}. hívó.")
+else:
+    print("Nem volt beszélő")
+
